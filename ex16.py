8ae0697cc05952454e1d5e2682645950d6b4a39b
Obdnanr <obdnanr@gmail.com>
2016-03-26 12:07:54 -0400

    fixed

--- ex16.py
+++ ex16.py
@@ -27,8 +27,9 @@ print "I'm going to write these to the file."
 target.write(line1)
 target.write("\n")
 target.write(line2)
-target.write("\")
+target.write("\n")
 target.write(line3)
+target.write("\n")
 
 print "and finally, we close it."
 target.close()
